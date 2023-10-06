4332f09ef3088c7967738a5e9b21b0e402721156
KaykSouza <kayksmr@gmail.com>
2023-10-06 16:23:11 -0300

    aula10

---
+++ aula10/script2.js
@@ -0,0 +1,10 @@
+function somar() {
+    const tn1 = document.getElementById('txtn1');
+    const tn2 = document.getElementById('txtn2');
+    const res = document.getElementById('res');
+    const n1 = Number(tn1.value);
+    const n2 = Number(tn2.value);
+    const s = n1 + n2;
+
+    res.innerHTML = `A soma entre ${n1} e ${n2} é igual a <stong>${s}<strong/>`
+}
