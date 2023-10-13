e9fc6052df422d8fb161406973cad7eba756f195
KaykSouza <kayksmr@gmail.com>
2023-10-13 12:25:18 -0300

    aula 11

---
+++ aula 11/ex009.js
@@ -0,0 +1,11 @@
+function validar() {
+    var txtpais = document.getElementById('txtp');
+    var res = document.querySelector('div#res');
+    res.innerHTML = `<p>O seu País é ${txtpais}`;
+
+    if(txtp === 'Brasil') {
+        res.innerHTML += `<p>Você é <strong>Brasileiro!</strong></p>`;
+    } else {
+        res.innerHTML += `<p>Você é <strong>Estrangeiro!</strong></p>`;
+    }
+}
