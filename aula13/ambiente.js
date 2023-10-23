387d4e3a47e8f90ce14adbffe3d11be0e2a69ec9
KaykSouza <kayksmr@gmail.com>
2023-10-23 16:23:06 -0300

    aula13

---
+++ aula13/ambiente.js
@@ -0,0 +1,16 @@
+// for(var contador = 1; contador <= 6; contador++) {
+//     console.log(`Passo ${contador}`)
+// }
+
+var contador = 1;
+do {
+    console.log(`Passo ${contador}`)
+    contador++
+} while (contador <= 6)
+
+// console.log('Olá tudo bem?')
+// console.log("Olá tudo bem?")
+// console.log("Olá tudo bem?")
+// console.log("Olá tudo bem?")
+// console.log("Olá tudo bem?")
+// console.log("Olá tudo bem?")
